caf892e7304abce678d14c1f922242dc4bb698e6
Charlon Palacay <charlon@gmail.com>
2018-10-29 15:36:38 -0700

    Card styling example.

--- src/myplan/home/index.jsx
+++ src/myplan/home/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Card, Image} from 'semantic-ui-react'
+import {Button, Card, Image, Feed} from 'semantic-ui-react'
 
 import './home.scss';
 
@@ -12,21 +12,85 @@ class Registration extends React.Component {
   render() {
     return (<div>
 
-       <Card>
-         <Card.Content>
-           <Card.Header>Registration</Card.Header>
-           <Card.Meta>Your priority registration date is not yet availble. Priority Registration goes from <strong>Nov. 2. 2018 - Nov. 18, 2018</strong></Card.Meta>
-           <Card.Description>Please refer to the <a href="">P1 Registration Dates</a> to find out when you can register for classes.</Card.Description>
-         </Card.Content>
-       </Card>
+      <Card fluid="fluid">
+        <Card.Content>
+          <Card.Header>Holds</Card.Header>
+        </Card.Content>
+        <Card.Content>
+          <Feed>
+            <Feed.Event>
+              <Feed.Label image='/images/avatar/small/jenny.jpg'/>
+              <Feed.Content>
+                <Feed.Date content='1 day ago'/>
+                <Feed.Summary>
+                  You added
+                  <a>Jenny Hess</a>
+                  to your
+                  <a>coworker</a>
+                  group.
+                </Feed.Summary>
+              </Feed.Content>
+            </Feed.Event>
 
-       <Card>
-         <Card.Content>
-           <Card.Header>Registration</Card.Header>
-           <Card.Meta>Your estimated Priority Registration date is <strong>Nov. 4. 2018 @ 6:00AM</strong></Card.Meta>
-           <Card.Description>How is you <a href="">P1 Registration Dates</a> calculated?</Card.Description>
-         </Card.Content>
-       </Card>
+            <Feed.Event>
+              <Feed.Label image='/images/avatar/small/molly.png'/>
+              <Feed.Content>
+                <Feed.Date content='3 days ago'/>
+                <Feed.Summary>
+                  You added
+                  <a>Molly Malone</a>
+                  as a friend.
+                </Feed.Summary>
+              </Feed.Content>
+            </Feed.Event>
+
+            <Feed.Event>
+              <Feed.Label image='/images/avatar/small/elliot.jpg'/>
+              <Feed.Content>
+                <Feed.Date content='4 days ago'/>
+                <Feed.Summary>
+                  You added
+                  <a>Elliot Baker</a>
+                  to your
+                  <a>musicians</a>
+                  group.
+                </Feed.Summary>
+              </Feed.Content>
+            </Feed.Event>
+          </Feed>
+        </Card.Content>
+      </Card>
+
+      <Card fluid>
+        <Card.Content>
+          <Card.Header>Registration</Card.Header>
+          <Card.Meta>From Nov. 2, 2018 to Nov 11, 2018</Card.Meta>
+          <Card.Description>
+            Your priority registration date is not yet availble. Priority Registration goes from
+            <strong>Nov. 2. 2018 - Nov. 18, 2018</strong>
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra="extra">
+          Please refer to the
+          <a href="">P1 Registration Dates</a>
+          to find out when you can register for classes.
+        </Card.Content>
+      </Card>
+
+      <Card fluid>
+        <Card.Content>
+          <Card.Header>Registration</Card.Header>
+          <Card.Meta>
+            <strong>Nov. 4. 2018 @ 6:00AM</strong>
+          </Card.Meta>
+          <Card.Description></Card.Description>
+        </Card.Content>
+        <Card.Content extra="extra">
+          How is your
+          <a href="">P1 Registration Dates</a>
+          calculated?
+        </Card.Content>
+      </Card>
 
     </div>);
   }

7da2a33171d014c0d18615b107773b5562e4eb67
Charlon Palacay <charlon@gmail.com>
2018-11-06 11:43:19 -0800

    Updating registration card styling.

--- src/myplan/registration/index.jsx
+++ src/myplan/registration/index.jsx
@@ -80,8 +80,11 @@ class Registration extends React.Component {
 
       <Segment color='red'>
         <Header as='h2'>Holds</Header>
-        <Label as='a' color='red' ribbon>Important!</Label>
-        <List relaxed>
+        <Message negative size='tiny'>
+          <Message.Header>You have holds!</Message.Header>
+          <p>The following registration holds will prevent you from being able to register.</p>
+        </Message>
+        <List divided relaxed>
           <List.Item>
             <List.Content>
               <List.Header>You have a hold on this.</List.Header>
@@ -186,7 +189,7 @@ class Registration extends React.Component {
 
           <Message negative size='tiny'>
             <Message.Header>We found problems with your plan.</Message.Header>
-            <p>The following courses/sections have issues that need to be addressed!</p>
+            <p>The following courses/sections have issues that need to be fixed!</p>
           </Message>
 
           <List divided relaxed>
@@ -221,87 +224,67 @@ class Registration extends React.Component {
           </List>
         </Segment>
 
+        <Segment.Group>
+          <Segment>
+            <Header as='h2'>Registration</Header>
 
+            <Label as='a' color='green' ribbon>Ready!</Label>
 
+            <List divided relaxed>
+              <List.Item>
+                <List.Content>
+                  <List.Header>CSE 142 Computer Programming I (4)</List.Header>
+                  <List.Description><Label color='gray' basic horizontal size='mini'>A</Label> KNE 130 Tue/Thur 8:30-9:50</List.Description>
+                  <List.Description><Label color='gray' basic horizontal size='mini'>AA</Label> KNE 130 Tue/Thur 8:30-9:50</List.Description>
+                </List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Content>
+                  <List.Header>CHEM 110 Preparation for General Chemistry (3-5)</List.Header>
+                  <List.Description><Label color='gray' basic horizontal size='mini'>A</Label> KNE 130 Tue/Thur 8:30-9:50</List.Description>
+                  <List.Description><Label color='gray' basic horizontal size='mini'>AA</Label> KNE 130 Tue/Thur 8:30-9:50</List.Description>
+                </List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Content>
+                  <List.Header>BIO A 370 Introduction to Primates (5)</List.Header>
+                  <List.Description><Label color='gray' basic horizontal size='mini'>A</Label> KNE 130 Tue/Thur 8:30-9:50</List.Description>
+                </List.Content>
+              </List.Item>
+            </List>
 
-        <Segment>
-          <Header as='h2'>Registration</Header>
+            <Message info size='small'>
+              <Message.Header>Priority Registration</Message.Header>
+              <p>Your est. registration date is not yet available. Priority registration starts on Nov. 2, 2018 @ 6am.</p>
+              <Button color="blue" disabled>Submit using MyPlan</Button>
+            </Message>
+            <Message info size='small'>
+              <Message.Header>Priority Registration</Message.Header>
+              <p>Your estimated priority registration date is <strong>Nov. 4, 2018</strong>.</p>
+              <Button color="blue" disabled>Submit using MyPlan</Button>
+            </Message>
+            <Message info size='small'>
+              <Message.Header>Priority Registration</Message.Header>
+              <p>Registration is on-going! MyPlan can submit your planned courses when it is time for your registration.</p>
+              <Button color="blue">Submit using MyPlan</Button>
+            </Message>
 
-          <Label as='a' color='green' ribbon>Ready!</Label>
-
-          <List divided relaxed>
-            <List.Item>
-              <List.Content floated="left">
-                <List.Header>CSE 142 AC</List.Header>
-                <List.Description>KNE 130 Tue/Thur 8:30-9:50</List.Description>
-              </List.Content>
-              <Button disabled size='mini'floated="right">View</Button>
-            </List.Item>
-            <List.Item>
-              <List.Content floated="left">
-                <List.Header>MATH 124 BB</List.Header>
-                <List.Description>KNE 130 Tue/Thur 8:30-9:50</List.Description>
-              </List.Content>
-              <Button disabled size='mini' floated="right">View</Button>
-            </List.Item>
-            <List.Item>
-              <List.Content floated="left">
-                <List.Header>BIO 200 AD</List.Header>
-                <List.Description>KNE 130 Tue/Thur 8:30-9:50</List.Description>
-              </List.Content>
-              <Button disabled size='mini' floated="right">View</Button>
-            </List.Item>
-            <List.Item>
-              <List.Content floated="left">
-                <List.Header>ENG 401 AA</List.Header>
-                <List.Description>KNE 130 Tue/Thur 8:30-9:50</List.Description>
-              </List.Content>
-              <Button disabled size='mini' floated="right">View</Button>
-            </List.Item>
-          </List>
-
-          <Message positive size='small'>
-            <Message.Header>Priority Registration</Message.Header>
-            <p>Your estimated priority registration date is <strong>Nov. 4, 2018</strong>.</p>
-            <Button color="green">Submit using MyPlan</Button>
-          </Message>
-
-          <Message size='mini'>
+          </Segment>
+          <Message size='tiny' attached='bottom'>
             <Message.Header>SPS Registration</Message.Header>
             <p>You may manually enter your SLN numbers using <a href='#'>SPS Registration</a>.</p>
           </Message>
+        </Segment.Group>
 
+        <Segment>
+          <Header as='h3'>Dropping Courses</Header>
+          <p>You cannot drop courses using MyPlan. If you need to update your schedule, visit SPS. <a href='#'>Drop courses using SPS</a>.</p>
         </Segment>
 
 
 
-      <Card fluid >
-        <Card.Content>
-          <Card.Header>Registration</Card.Header>
-        </Card.Content>
-        <Card.Content>
-          <Card.Description>
-            <p>Your priority registration date is not yet availble. Priority Registration goes from <strong>Nov. 2. 2018 - Nov. 18, 2018</strong>.</p>
-          </Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          Please refer to <a href="">P1 Registration Dates</a> to find out when you can register for classes.
-        </Card.Content>
-      </Card>
 
-      <Card fluid>
-        <Card.Content>
-          <Card.Header>Registration</Card.Header>
-        </Card.Content>
-        <Card.Content>
-          <Card.Description>
-            <p>Your estimated priority registration date is <strong>Nov. 4, 2018</strong>.</p>
-          </Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          How is your <a href="">P1 Registration Date</a> calculated?
-        </Card.Content>
-      </Card>
+
 
       <p>This is a Panel component using React-Bootstrap v3.</p>
 

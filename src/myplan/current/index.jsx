2b97a99834fa430e3d02b2e4d0de8060c43fcf85
Charlon Palacay <charlon@gmail.com>
2018-11-09 18:37:14 -0800

    Flesh out student profile card.

--- src/myplan/current/index.jsx
+++ src/myplan/current/index.jsx
@@ -53,12 +53,15 @@ class Current extends React.Component {
         <Col md={4}>
 
           <Card border='light' className='shadow-sm mb-4'>
-            <Card.Img variant="top" src="https://myplan-dev.washington.edu/home/images/6.png" />
+            <Card.Img variant="top" src="https://myplan-dev.washington.edu/home/images/6.png" className='mp-user-background' />
             <Card.Body>
+              <Image src="https://randomuser.me/api/portraits/lego/1.jpg" roundedCircle className='shadow-sm mp-user' />
               <Card.Title as='h4'>Hello, J. Julius!</Card.Title>
-              <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc turpis quam, euismod vel dolor ut, rhoncus volutpat orci. Morbi blandit finibus neque, ac ullamcorper mauris.</Card.Text>
-              <Card.Link href="#">View Profile</Card.Link>
-              <Card.Link href="#">Hide Forever!</Card.Link>
+              <Card.Text>
+                <p>Class:<br/>Freshman</p>
+                <p>Intended Major:<br/>Bachelor of Arts Business</p>
+              </Card.Text>
+              <Card.Link href="#">View Academic History (audit)</Card.Link>
             </Card.Body>
           </Card>
 

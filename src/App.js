fd20099bb9b1d2272653487ad0b1022ae6257f95
Charlon Palacay <charlon@gmail.com>
2018-11-15 20:37:24 -0800

    Adds previous page component.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Courses from './myplan/pages/Courses'
 import Programs from './myplan/pages/Programs'
 
 import Current from './myplan/current/'
+import Previous from './myplan/pages/Previous'
 import Preview from './myplan/preview/'
 import Future from './myplan/future/'
 
@@ -18,10 +19,11 @@ const App = () => (
     <Router>
       <Switch>
         <Route exact={true} path="/" component={Current} />
-        <Route path="/programs" component={Programs} />
-        <Route path="/courses" component={Courses} />
+        <Route path="/previous" component={Previous} />
         <Route path="/preview" component={Preview} />
         <Route path="/future" component={Future} />
+        <Route path="/programs" component={Programs} />
+        <Route path="/courses" component={Courses} />
       </Switch>
     </Router>
   </MyPlan>
---
+++ src/myplan/pages/Previous.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Card, CardDeck, Row, Col, ListGroup}  from 'react-bootstrap'
+
+import './Previous.scss';
+
+class Previous extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount(){
+    document.title = "Summer 2018"
+  }
+
+  render() {
+    return (
+      <div>
+
+      <Row className='mb-4'>
+        <Col xs={6}>
+          <span className="badge badge-secondary">Previous</span>
+          <h1 className='mt-0 text-nowrap'>Summer 2018</h1>
+        </Col>
+        <Col xs={6}>
+
+          <Row>
+            <Col xs={4}>&nbsp;</Col>
+            <Col xs={4} className='text-right'>
+              <span className="badge badge-light"> -- Previous</span>
+              <h6 className='mt-0 font-weight-light text-nowrap'><a href='/preview'>Spring 2018</a></h6>
+            </Col>
+            <Col xs={4}>
+              <span className="badge badge-light">Next --</span>
+              <h6 className='mt-0 font-weight-light text-nowrap'><a href='/'>Autumn 2018</a></h6>
+            </Col>
+          </Row>
+
+        </Col>
+      </Row>
+
+      </div>
+    );
+  }
+}
+export default Previous

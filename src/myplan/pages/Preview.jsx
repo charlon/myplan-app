ad90c134dd9b815082c5667db1af93295f80c5a1
Charlon Palacay <charlon@gmail.com>
2018-12-19 10:47:53 -0800

    Add responsive table for section list details.

--- src/myplan/pages/Preview.jsx
+++ src/myplan/pages/Preview.jsx
@@ -1,7 +1,7 @@
 import 'rc-steps/assets/index.css';
 import 'rc-steps/assets/iconfont.css';
 import React from 'react';
-import { Card, CardDeck, Row, Col, Alert, Button, ListGroup}  from 'react-bootstrap';
+import { Card, CardDeck, Row, Col, Alert, Button, ListGroup, Table}  from 'react-bootstrap';
 import Steps from 'rc-steps';
 
 import UserProfile from '../components/UserProfile.jsx';
@@ -80,60 +80,11 @@ class Preview extends React.Component {
             </Card.Body>
           </Card>
 
-
-
-        </Col>
-        <Col md={8}>
-
-        <Card className='shadow-sm mb-4'>
-          <Card.Body>
-            <Card.Title as='h4'>Scheduled Courses</Card.Title>
-
-            <ListGroup variant='flush' as='ul' className='mb-4'>
-              <ListGroup.Item as="li" className='pl-0 pr-0 border-top-0'>
-                <a href="#" className="list-group-item-action">
-                  <div className="d-flex w-100 justify-content-between">
-                    <h5 className="mb-1">CSE 142 Computer Programming I </h5>
-                    <small className='text-nowrap'>5 credits</small>
-                  </div>
-                  <ul className='list-unstyled'>
-                    <li>A KNE 130 Tue/Thur 8:30-9:50</li>
-                    <li>AC KNE 130 Tue/Thur 8:30-9:50</li>
-                  </ul>
-                </a>
-              </ListGroup.Item>
-              <ListGroup.Item as="li" className='pl-0 pr-0 border-bottom-0'>
-                <a href="#" className="list-group-item-action">
-                  <div className="d-flex w-100 justify-content-between">
-                    <h5 className="mb-1">GEN ST 199 A2: University Community</h5>
-                    <small className='text-nowrap'>12 credits</small>
-                  </div>
-                  <ul className='list-unstyled'>
-                    <li>GEN ST 199 A2: University Community TH 1:30-2:20</li>
-                    <li>CHEM 142 C: General Chemistry MWF 12:30-1:20</li>
-                    <li>CHEM 142 CD T 9:30-10:20 TH 2:30-5:20</li>
-                    <li>ENGL 199 H: Linked Writing Course: HSERVE 100 MWF 8:30-9:20</li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                  </ul>
-                </a>
-              </ListGroup.Item>
-            </ListGroup>
-
-            <h4 className='text-right'>Currently Enrolled: 17 credits</h4>
-
-            <Card.Link href="#">View MyUW</Card.Link>
-          </Card.Body>
-        </Card>
-
           <Card className='shadow-sm mb-4'>
             <Card.Body>
-              <Card.Title as='h3'>Issues</Card.Title>
+              <Card.Title as='h4'>Scheduled Courses</Card.Title>
 
-              <Alert variant='danger' className='mb-0'>These courses are ready!</Alert>
-
-              <ListGroup variant='flush' as="ul">
+              <ListGroup variant='flush' as='ul' className='mb-4'>
                 <ListGroup.Item as="li" className='pl-0 pr-0 border-top-0'>
                   <a href="#" className="list-group-item-action">
                     <div className="d-flex w-100 justify-content-between">
@@ -144,6 +95,85 @@ class Preview extends React.Component {
                       <li>A KNE 130 Tue/Thur 8:30-9:50</li>
                       <li>AC KNE 130 Tue/Thur 8:30-9:50</li>
                     </ul>
+                  </a>
+                </ListGroup.Item>
+                <ListGroup.Item as="li" className='pl-0 pr-0 border-bottom-0'>
+                  <a href="#" className="list-group-item-action">
+                    <div className="d-flex w-100 justify-content-between">
+                      <h5 className="mb-1">GEN ST 199 A2: University Community</h5>
+                      <small className='text-nowrap'>12 credits</small>
+                    </div>
+                    <ul className='list-unstyled'>
+                      <li>GEN ST 199 A2: University Community TH 1:30-2:20</li>
+                      <li>CHEM 142 C: General Chemistry MWF 12:30-1:20</li>
+                      <li>CHEM 142 CD T 9:30-10:20 TH 2:30-5:20</li>
+                      <li>ENGL 199 H: Linked Writing Course: HSERVE 100 MWF 8:30-9:20</li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                    </ul>
+                  </a>
+                </ListGroup.Item>
+              </ListGroup>
+
+              <h4 className='text-right'>Currently Enrolled: 17 credits</h4>
+
+              <Card.Link href="#">View MyUW</Card.Link>
+            </Card.Body>
+          </Card>
+
+
+        </Col>
+        <Col md={8}>
+
+
+
+          <Card className='shadow-sm mb-4'>
+            <Card.Body>
+              <Card.Title as='h3'>Issues</Card.Title>
+
+              <Alert variant='danger' className='mb-0'>These courses have issues!</Alert>
+
+              <ListGroup variant='flush' as="ul">
+                <ListGroup.Item as="li" className='pl-0 pr-0 border-top-0'>
+                  <a href="#" className="list-group-item-action">
+                    <div className="d-flex w-100 justify-content-between">
+                      <h5 className="mb-1">CSE 142 Computer Programming I </h5>
+                      <small className='text-nowrap'>5 credits</small>
+                    </div>
+                    <Table responsive condensed>
+                      <thead>
+                        <tr>
+                          <th>Section</th>
+                          <th>Location</th>
+                          <th>Time</th>
+                          <th>Table heading</th>
+                          <th>Table heading</th>
+                          <th>Table heading</th>
+                          <th>Table heading</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>A</td>
+                          <td>KNE 130</td>
+                          <td>Tue/Thur</td>
+                          <td>Table cell</td>
+                          <td>Table cell</td>
+                          <td>Table cell</td>
+                          <td>Table cell</td>
+                        </tr>
+                        <tr>
+                          <td>AC</td>
+                          <td>KNE 130</td>
+                          <td>Table cell</td>
+                          <td>Table cell</td>
+                          <td>Table cell</td>
+                          <td>Table cell</td>
+                          <td>Table cell</td>
+                        </tr>
+                      </tbody>
+                    </Table>
                   </a>
                 </ListGroup.Item>
                 <ListGroup.Item as="li" className='pl-0 pr-0'>
